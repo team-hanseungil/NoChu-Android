d7c4e055383778703678f3888c9ba58fdad00de1
jsw9436 <jswjsw101010@gmail.com>
2026-06-25 15:08:22 +0900

    :sparkles: :: add spotify api

--- core/network/src/main/java/com/school_of_company/network/api/AuthAPI.kt
+++ core/network/src/main/java/com/school_of_company/network/api/AuthAPI.kt
@@ -1,56 +1,35 @@
 package com.school_of_company.network.api
 
-import com.school_of_company.network.dto.reponse.LoginResponse
-import com.school_of_company.network.dto.auth.requset.LoginRequest
-import com.school_of_company.network.dto.auth.requset.SignUpCertificationNumberSendRequest
-import com.school_of_company.network.dto.auth.requset.SignUpRequest
-import com.school_of_company.network.dto.auth.requset.SmsVerifyCodeRequest
-import com.school_of_company.network.dto.member.response.GetAllMemberResponse
+import com.school_of_company.network.dto.reponse.TokenResponse
+import com.school_of_company.network.dto.auth.requset.SpotifyLoginRequest
+import com.school_of_company.network.dto.auth.requset.RefreshTokenRequest
 import com.school_of_company.network.dto.reponse.EmotionResponse
 import com.school_of_company.network.dto.reponse.PlaylistResponse
-import dagger.Module
 import okhttp3.MultipartBody
 import retrofit2.http.Body
 import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Multipart // Header import 추가
-import retrofit2.http.PATCH
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface AuthAPI {
-    @POST("/api/auth/signup")
-    suspend fun signUp(
-        @Body body: SignUpRequest
-    )
 
-    @POST("/api/auth/signin")
-    suspend fun login(
-        @Body body: SignUpRequest
-    ): LoginResponse
+    @POST("/auth/spotify")
+    suspend fun loginWithSpotify(
+        @Body body: SpotifyLoginRequest
+    ): TokenResponse
 
-    // 💡 수정된 부분: @Header 인자를 추가하여 RefreshToken을 명시적으로 전달
-    @PATCH("/api/auth/reissue")
-    suspend fun tokenRefresh(): LoginResponse
+    @POST("/auth/refresh")
+    suspend fun refreshToken(
+        @Body body: RefreshTokenRequest
+    ): TokenResponse
 
     @DELETE("/api/auth/signout")
     suspend fun logout()
 
     @DELETE("/api/auth/out")
     suspend fun signLogout()
-
-    @POST("/api/sms")
-    suspend fun signUpCertificationNumberSend(
-        @Body body: SignUpCertificationNumberSendRequest
-    )
-
-    @POST("/api/sms/verify")
-    suspend fun signUpCertificationNumberCertification(
-        @Body body: SmsVerifyCodeRequest
-    )
 
     @Multipart
     @POST("/api/emotions/{memberId}")
@@ -63,6 +42,4 @@ interface AuthAPI {
     suspend fun musicRR(
         @Path("memberId") memberId: Long,
     ): PlaylistResponse
-
-
 }
